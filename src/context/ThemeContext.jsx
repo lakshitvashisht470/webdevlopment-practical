2377e5293ce39fe7c5c9b20108b220b15e435c07
Lakshit <lakshitvashisht470@gmail.com>
2026-05-07 09:56:29 +0530

    Add files via upload

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,12 @@
+import React, { createContext, useState } from 'react'
+
+export const ThemeContext = createContext()
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState('light')
+  const toggleTheme = () => setTheme((t) => (t === 'light' ? 'dark' : 'light'))
+
+  const value = { theme, toggleTheme }
+
+  return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+}
